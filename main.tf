c9f154b3b402f450c9e57e5156c0395c27f3801d
Awil360 <quule360@gmail.com>
2024-05-21 08:44:44 +0530

    map pubic ip

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "aws_subnet" "crm-web-sn" {
     Name = "crm-web-subnet"
   }
 }
+# api subnet
+resource "aws_subnet" "crm-api-sn" {
+  vpc_id     = aws_vpc.crm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "crm-api-subnet"
+  }
+}
